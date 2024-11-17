5a498657aef59ef4e31bcc9edf2888ce411fc45b
Fiza Malik <fizamalik1314@gmail.com>
2024-11-17 15:15:28 +0000

    Remove heading link icons and Vercel toolbar

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -13,14 +13,8 @@ const md = new MarkdownIt({
   linkify: true,
   typographer: true,
 }).use(anchor, {
-  permalink: anchor.permalink.ariaHidden({
-    symbol: `<svg class="anchor-icon ml-2 h-4 w-4 opacity-0 group-hover:opacity-100" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-              <path fillRule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
-            </svg>`,
-    placement: 'before',
-    class: 'group'
-  }),
-  level: [1, 2, 3]
+  permalink: false,
+  slugify: (s: string) => s.toLowerCase().replace(/[^\w\s-]/g, '').replace(/\s+/g, '-')
 });
 
 export function getPostBySlug(slug: string): Post {
